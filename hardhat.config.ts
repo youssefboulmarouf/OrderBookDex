d6f8aefaa4a58664fc28293fc68c5f0c89f5ce75
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-01-30 19:24:38 -0500

    chagned artifact location

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,9 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  paths: {
+    artifacts: 'client/src/artifacts'
+  }
 };
 
 export default config;
