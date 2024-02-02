352a24b0e5f885090f92a70b812fae9dbc5d1a93
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-02-01 22:01:43 -0500

    added more methods to contract class

--- client/src/services/OrderBookDexContract.tsx
+++ client/src/services/OrderBookDexContract.tsx
@@ -1,19 +1,55 @@
-import { ethers, Contract } from 'ethers';
+import { ethers, Contract, Signer } from 'ethers';
+import OrderBookDex from '../artifacts/contracts/OrderBookDex.sol/OrderBookDex.json';
+import { TokenProps } from '../components/common/common-props';
 
 class OrderBookDexContract {
     private contract: Contract;
 
     constructor(provider: ethers.providers.Provider, contractAddress: string) {
-        this.contract = new ethers.Contract(contractAddress, "YourContractABI", provider);
+        this.contract = new ethers.Contract(contractAddress, OrderBookDex.abi, provider);
     }
 
-    async getContract(): Promise<string> {
+    getContractAddress(): string {
+        return this.contract.address;
+    }
+
+    async getAllTokens(): Promise<TokenProps[]> {
+        return await this.contract.getTokens();
+    }
+
+    async addToken(token: TokenProps, signer: Signer): Promise<void> {
         try {
-            const value = await this.contract.getValue();
-            return value.toString();
-        } catch (error) {
-            console.error('Error in getContractValue:', error);
-            throw error;
+            const tx =  await this.contract
+                .connect(signer)
+                .addToken(
+                    ethers.utils.formatBytes32String(token.ticker),
+                    token.tokenAddress
+                );
+            await tx.wait();
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    async disableToken(token: TokenProps, signer: Signer): Promise<void> {
+        try {
+            const tx =  await this.contract
+                .connect(signer)
+                .disableTokenTrading(token.ticker);
+            await tx.wait();
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    async enableToken(token: TokenProps, signer: Signer): Promise<void> {
+        try {
+            const tx =  await this.contract
+                .connect(signer)
+                .enableTokenTrading(token.ticker);
+            await tx.wait();
+        } catch (e) {
+            console.error(e)
         }
     }
 }
