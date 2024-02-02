e2e7b5a706170093ee413d31ef471d5619c94b62
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-02-01 22:01:03 -0500

    admin section to add, enable and disable token

--- client/src/components/admin-section/AdminSection.tsx
+++ client/src/components/admin-section/AdminSection.tsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, FormGroup, Table } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { TokenProps } from '../common/common-props';
-import { PlusSquareFill, XSquareFill } from 'react-bootstrap-icons';
+import { CheckCircleFill, PlusSquareFill, XCircleFill, XSquareFill } from 'react-bootstrap-icons';
+import { ethers, Signer } from 'ethers';
+import OrderBookDexContract from '../../services/OrderBookDexContract';
 
 interface AdminSectionProps {
     showAdminSection: boolean; 
     handleClose: () => void;
+    orderBookDexContract: OrderBookDexContract;
+    account: Signer;
 }
 
 const AdminSection: React.FC<AdminSectionProps> = (props) => {
-    const [token, setToken] = useState<TokenProps>({ ticker: '', address: '', isTradable: true });
+    const [token, setToken] = useState<TokenProps>({ ticker: '', tokenAddress: '', isTradable: true });
+    const [tokens, setTokens] = useState<TokenProps[]>([]);
     
+    const loadTokens = async () => {
+        const allTokens: TokenProps[] = await props.orderBookDexContract.getAllTokens();
+        setTokens(allTokens);
+    };
+
     const handleTokenChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         setToken(prevToken => ({
@@ -21,13 +31,35 @@ const AdminSection: React.FC<AdminSectionProps> = (props) => {
         }));
     };
     
-    const handleAddToken = (e: React.FormEvent) => {
+    const addToken = async (e: React.FormEvent) => {
         e.preventDefault();
-        console.log("Token:", token);
+        await props.orderBookDexContract.addToken(token, props.account);
+        await loadTokens();
     }
 
+    const enableToken = async (e: React.FormEvent, token: TokenProps) => {
+        e.preventDefault();
+        console.log('ENABLE: ', token, e.target);
+        await props.orderBookDexContract.enableToken(token, props.account);
+        await loadTokens();
+    }
+
+    const disableToken = async (e: React.FormEvent, token: TokenProps) => {
+        e.preventDefault();
+        console.log('DISABLE: ', token, e.target);
+        await props.orderBookDexContract.disableToken(token, props.account);
+        await loadTokens();
+    }
+
+    useEffect(() => {
+        const initToken = async () => {
+            await loadTokens();
+        };
+        initToken();
+    }, []);
+
     return (
-        <Offcanvas show={props.showAdminSection} onHide={props.handleClose}>
+        <Offcanvas show={props.showAdminSection} onHide={props.handleClose} placement='bottom'>
             <Offcanvas.Header closeButton>
                 <Offcanvas.Title>Admin Section</Offcanvas.Title>
             </Offcanvas.Header>
@@ -35,7 +67,7 @@ const AdminSection: React.FC<AdminSectionProps> = (props) => {
                 <div>
                     <h4>Add A New Token</h4>
                 </div>
-                <Form onSubmit={(e) => {handleAddToken(e)}}>
+                <Form onSubmit={(e) => {addToken(e)}}>
                     <FormGroup>
                         <Form.Control 
                             size='sm'
@@ -52,8 +84,8 @@ const AdminSection: React.FC<AdminSectionProps> = (props) => {
                             size='sm'
                             type='text'
                             placeholder='Token Address'
-                            name='address'
-                            value={token?.address}
+                            name='tokenAddress'
+                            value={token?.tokenAddress}
                             onChange={handleTokenChange}
                         />
                     </FormGroup>
@@ -69,29 +101,44 @@ const AdminSection: React.FC<AdminSectionProps> = (props) => {
                     <thead>
                         <tr>
                             <th>Ticker</th>
-                            <th>Address</th>
-                            <th>Action</th>
+                            <th>HEX Ticker</th>
+                            <th>Token Address</th>
+                            <th>Status</th>
+                            <th>Enable/Disable</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>
-                                <Button className='action-button' variant=''>
-                                    <XSquareFill color='red' size={20}/>
-                                </Button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>
-                                <Button className='action-button' variant=''>
-                                    <PlusSquareFill color='green' size={20}/>
-                                </Button>
-                            </td>
-                        </tr>
+                        {tokens.map(token => (
+                            <tr key={token.ticker}>
+                                <td>{ethers.utils.parseBytes32String(token.ticker)}</td>
+                                <td>{token.ticker}</td>
+                                <td>{token.tokenAddress}</td>
+                                <td>
+                                    {(token.isTradable)
+                                        ? <CheckCircleFill color='green' size={20}/>
+                                        : <XCircleFill color='red' size={20}/>
+                                    }
+                                </td>
+                                <td>
+                                    {(token.isTradable)
+                                        ? <Button 
+                                            className='action-button' 
+                                            variant='' 
+                                            onClick={(e) => disableToken(e, token)}
+                                        >
+                                            <XSquareFill color='red' size={20}/>
+                                        </Button>
+                                        : <Button 
+                                            className='action-button' 
+                                            variant='' 
+                                            onClick={(e) => enableToken(e, token)}
+                                         >
+                                            <PlusSquareFill color='green' size={20}/>
+                                        </Button>
+                                    }
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </Table>
 
