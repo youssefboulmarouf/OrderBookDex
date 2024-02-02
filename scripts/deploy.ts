121a4c18029c040213359249a1298ebda2c1c461
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-02-01 21:59:13 -0500

    added tokens to dex contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,17 +2,26 @@ import { ethers } from "hardhat";
 import fs from 'fs';
 
 async function main() {
+    // Deploy DAI Token
     const daiToken = await ethers.deployContract("TestToken", ['Dai Stable Coin', 'DAI'])
     await daiToken.waitForDeployment();
     const daiAddress = await daiToken.getAddress();
 
+    // Deploy ZRX Token
     const zrxToken = await ethers.deployContract("TestToken", ['Xero Token', 'ZRX'])
     await zrxToken.waitForDeployment();
     const zrxAddress = await zrxToken.getAddress();
 
+    // Deploy Order Book Dex
     const orderBookDex = await ethers.deployContract("OrderBookDex");
     await orderBookDex.waitForDeployment();
     const orderBookDexAddress = await orderBookDex.getAddress();
+
+    // Add DAI token
+    await orderBookDex.addToken(ethers.encodeBytes32String('DAI'), daiAddress);
+    
+    // Add ZRX token
+    await orderBookDex.addToken(ethers.encodeBytes32String('ZRX'), zrxAddress);
 
     const adresses = {
         OBDex: orderBookDexAddress,
