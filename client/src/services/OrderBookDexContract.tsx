d8e29f106657beb994879f522e7149825e860881
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-02-10 20:32:02 -0500

    added wuote token methods

--- client/src/services/OrderBookDexContract.tsx
+++ client/src/services/OrderBookDexContract.tsx
@@ -1,6 +1,7 @@
 import { ethers, Contract, Signer } from 'ethers';
 import OrderBookDex from '../artifacts/contracts/OrderBookDex.sol/OrderBookDex.json';
 import { TokenProps, TokenDexBalance } from '../components/common/common-props';
+import Utils from '../utils';
 
 class OrderBookDexContract {
     private contract: Contract;
@@ -13,71 +14,80 @@ class OrderBookDexContract {
         return this.contract.getAddress();
     }
 
+    async getQuoteTicker(): Promise<string> {
+        const quoteTicker = await this.contract.quoteTicker();
+        return quoteTicker;
+    }
+
+    async setQuoteTicker(token: TokenProps): Promise<void> {
+        try {
+            const tx =  await this.contract.setQuoteTicker(token.ticker);
+            await tx.wait();
+        } catch (e) {
+            Utils.handleError(e)
+        }
+    }
+
     async getAllTokens(): Promise<TokenProps[]> {
-        const tokens = await this.contract.getTokens();
-        console.log('tokens: ', tokens);
-        return tokens;
+        return await this.contract.getTokens();
     }
 
     async addToken(token: TokenProps): Promise<void> {
         try {
-            const tx =  await this.contract
-                .addToken(
-                    ethers.encodeBytes32String(token.ticker),
-                    token.tokenAddress
-                );
+            const tx =  await this.contract.addToken(
+                ethers.encodeBytes32String(token.ticker),
+                token.tokenAddress
+            );
             await tx.wait();
         } catch (e) {
-            console.error(e)
+            Utils.handleError(e)
         }
     }
 
     async disableToken(token: TokenProps): Promise<void> {
         try {
-            const tx =  await this.contract
-                .disableTokenTrading(token.ticker);
+            const tx =  await this.contract.disableTokenTrading(token.ticker);
             await tx.wait();
         } catch (e) {
-            console.error(e)
+            Utils.handleError(e)
         }
     }
 
     async enableToken(token: TokenProps): Promise<void> {
         try {
-            const tx =  await this.contract
-                .enableTokenTrading(token.ticker);
+            const tx =  await this.contract.enableTokenTrading(token.ticker);
             await tx.wait();
         } catch (e) {
-            console.error(e)
+            Utils.handleError(e)
         }
     }
 
     async getBalance(token: TokenProps, signer: Signer): Promise<TokenDexBalance | undefined> {
         try {
-            return await this.contract
-                .balances(await signer.getAddress(), token.ticker);
+            return await this.contract.balances(
+                await signer.getAddress(), 
+                token.ticker
+            );
         } catch (e) {
-            console.error(e)
+            Utils.handleError(e)
         }
     }
 
     async deposit(token: TokenProps, amount: BigInt): Promise<void> {
         try {
-            const tx = await this.contract
-                .deposit(token.ticker, amount);
+            const tx = await this.contract.deposit(token.ticker, amount);
             await tx.wait();
         } catch (e) {
-            console.error(e)
+            Utils.handleError(e)
         }
     }
 
     async withdraw(token: TokenProps, amount: BigInt): Promise<void> {
         try {
-            const tx = await this.contract
-                .withdraw(token.ticker, amount);
+            const tx = await this.contract.withdraw(token.ticker, amount);
             await tx.wait();
         } catch (e) {
-            console.error(e)
+            Utils.handleError(e)
         }
     }
 }
