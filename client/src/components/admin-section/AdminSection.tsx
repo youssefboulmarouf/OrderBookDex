48ed4a052d80f0dce326b4de67b81a565f542efd
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-02-10 20:30:53 -0500

    simplified admin section and added some css

--- client/src/components/admin-section/AdminSection.tsx
+++ client/src/components/admin-section/AdminSection.tsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Form, FormGroup, Table } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
+
+import './admin-section.css';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { TokenProps } from '../common/common-props';
-import { CheckCircleFill, PlusSquareFill, XCircleFill, XSquareFill } from 'react-bootstrap-icons';
-import { ethers, Signer } from 'ethers';
 import OrderBookDexContract from '../../services/OrderBookDexContract';
+import AddNewToken from './AddNewToken';
+import AdQuoteToken from './AddQuoteToken';
+import TokensList from './TokensList';
 
 interface AdminSectionProps {
     showAdminSection: boolean; 
     handleClose: () => void;
     orderBookDexContract: OrderBookDexContract;
-    account: Signer;
 }
 
 const AdminSection: React.FC<AdminSectionProps> = (props) => {
@@ -19,8 +23,7 @@ const AdminSection: React.FC<AdminSectionProps> = (props) => {
     const [tokens, setTokens] = useState<TokenProps[]>([]);
     
     const loadTokens = async () => {
-        const allTokens: TokenProps[] = await props.orderBookDexContract.getAllTokens();
-        setTokens(allTokens);
+        setTokens(await props.orderBookDexContract.getAllTokens());
     };
 
     const handleTokenChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,27 +38,26 @@ const AdminSection: React.FC<AdminSectionProps> = (props) => {
         e.preventDefault();
         await props.orderBookDexContract.addToken(token);
         await loadTokens();
+        setToken({ ticker: '', tokenAddress: '', isTradable: true });
     }
 
     const enableToken = async (e: React.FormEvent, token: TokenProps) => {
         e.preventDefault();
-        console.log('ENABLE: ', token, e.target);
         await props.orderBookDexContract.enableToken(token);
         await loadTokens();
     }
 
     const disableToken = async (e: React.FormEvent, token: TokenProps) => {
         e.preventDefault();
-        console.log('DISABLE: ', token, e.target);
         await props.orderBookDexContract.disableToken(token);
         await loadTokens();
     }
 
     useEffect(() => {
-        const initToken = async () => {
+        const loadData = async () => {
             await loadTokens();
         };
-        initToken();
+        loadData();
     }, []);
 
     return (
@@ -64,84 +66,29 @@ const AdminSection: React.FC<AdminSectionProps> = (props) => {
                 <Offcanvas.Title>Admin Section</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                <div>
-                    <h4>Add A New Token</h4>
-                </div>
-                <Form onSubmit={(e) => {addToken(e)}}>
-                    <FormGroup>
-                        <Form.Control 
-                            size='sm'
-                            type='text'
-                            placeholder='Token Ticker'
-                            name='ticker'
-                            value={token?.ticker}
-                            onChange={handleTokenChange}
+                <Row>
+                    <Col sm={6}>
+                        <AddNewToken 
+                            token={token} 
+                            addToken={addToken} 
+                            handleTokenChange={handleTokenChange}
                         />
-                    </FormGroup>
-                    <br />
-                    <FormGroup>
-                        <Form.Control 
-                            size='sm'
-                            type='text'
-                            placeholder='Token Address'
-                            name='tokenAddress'
-                            value={token?.tokenAddress}
-                            onChange={handleTokenChange}
+                    </Col>
+                    <Col sm={6}>
+                        <AdQuoteToken 
+                            tokens={tokens} 
+                            orderBookDexContract={props.orderBookDexContract}
                         />
-                    </FormGroup>
-                    <br />
-                    <Button type="submit">Add Token</Button>
-                </Form>
-
-                <br />
-                <div>
-                    <h4>Tokens List</h4>
-                </div>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Ticker</th>
-                            <th>HEX Ticker</th>
-                            <th>Token Address</th>
-                            <th>Status</th>
-                            <th>Enable/Disable</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {tokens.map(token => (
-                            <tr key={token.ticker}>
-                                <td>{ethers.decodeBytes32String(token.ticker)}</td>
-                                <td>{token.ticker}</td>
-                                <td>{token.tokenAddress}</td>
-                                <td>
-                                    {(token.isTradable)
-                                        ? <CheckCircleFill color='green' size={20}/>
-                                        : <XCircleFill color='red' size={20}/>
-                                    }
-                                </td>
-                                <td>
-                                    {(token.isTradable)
-                                        ? <Button 
-                                            className='action-button' 
-                                            variant='' 
-                                            onClick={(e) => disableToken(e, token)}
-                                        >
-                                            <XSquareFill color='red' size={20}/>
-                                        </Button>
-                                        : <Button 
-                                            className='action-button' 
-                                            variant='' 
-                                            onClick={(e) => enableToken(e, token)}
-                                         >
-                                            <PlusSquareFill color='green' size={20}/>
-                                        </Button>
-                                    }
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-
+                    </Col>
+                </Row>
+                <br/>
+                <Row>
+                    <TokensList 
+                        tokens={tokens}
+                        enableToken={enableToken}
+                        disableToken={disableToken}
+                    />
+                </Row>
             </Offcanvas.Body>
         </Offcanvas>
     );
