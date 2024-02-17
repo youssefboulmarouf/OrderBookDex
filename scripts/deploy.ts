15d9da0194d960ae89bc8f9fb129f959cf8d0691
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-02-16 21:13:39 -0500

    placed orders in deployement

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,35 +4,45 @@ import { OrderBookDex, TestToken } from "../typechain-types";
 import { Signer } from "ethers";
 
 const deployToken = async (name: string, symbol: string): Promise<TestToken> => {
+    console.log('Deploying Token: ', name);
     const token = await ethers.deployContract("TestToken", [name, symbol])
     await token.waitForDeployment();
     return token;
 }
 
 const seedAccount = async (orderBookDex: OrderBookDex, tokens: TestToken[], trader: Signer) => {
-    tokens.map(async token => {
-        const amountToMint =  ethers.parseUnits('1000', 'ether');
+    await Promise.all(
+        tokens.map(async token => {
+            const tokenName = await token.symbol();
+            const ticker = ethers.encodeBytes32String(tokenName);
+            const amountToMint =  ethers.parseUnits('1000', 'ether');
+    
+            await token.faucet(trader, amountToMint);
+            const orderBookDexAddress = await orderBookDex.getAddress();
+            await token.connect(trader).approve(orderBookDexAddress, amountToMint);
+    
+            const amountToDeposit =  ethers.parseUnits('250', 'ether');
+            await orderBookDex.connect(trader).deposit(ticker, amountToDeposit);
 
-        await token.faucet(trader, amountToMint);
-        const orderBookDexAddress = await orderBookDex.getAddress();
-        await token.connect(trader).approve(orderBookDexAddress, amountToMint);
-
-        const tokenName = await token.symbol();
-        const ticker = ethers.encodeBytes32String(tokenName);
-
-        const amountToDeposit =  ethers.parseUnits('250', 'ether');
-        await orderBookDex.connect(trader).deposit(ticker, amountToDeposit);
-    });
+            console.log('Seeding Trader Account: ', tokenName, await trader.getAddress());
+            console.log('Amount: ', amountToMint, amountToDeposit);
+        })
+    );
 }
 
 async function main() {
+    const ORDER_SIDE = {BUY: 0, SELL: 1};
+    const ORDER_TYPE = {MARKET: 0, LIMIT: 1};
+
     // Deploy DAI Token
     const daiToken: TestToken = await deployToken('Dai Stable Coin', 'DAI');
-    const daiAddress = await daiToken.getAddress();
+    const daiAddress: string = await daiToken.getAddress();
+    const daiSymbol: string = await daiToken.symbol();
 
     // Deploy ZRX Token
     const zrxToken: TestToken = await deployToken('Xero Token', 'ZRX');
-    const zrxAddress = await zrxToken.getAddress();
+    const zrxAddress: string = await zrxToken.getAddress();
+    const zrxSymbol: string = await zrxToken.symbol();
 
     // Deploy Order Book Dex
     const orderBookDex = await ethers.deployContract("OrderBookDex");
@@ -40,13 +50,53 @@ async function main() {
     const orderBookDexAddress = await orderBookDex.getAddress();
 
     // Add DAI and ZRX tokens
-    await orderBookDex.addToken(ethers.encodeBytes32String('DAI'), daiAddress);
-    await orderBookDex.addToken(ethers.encodeBytes32String('ZRX'), zrxAddress);
+    await orderBookDex.addToken(ethers.encodeBytes32String(daiSymbol), daiAddress);
+    await orderBookDex.addToken(ethers.encodeBytes32String(zrxSymbol), zrxAddress);
 
-    const [owner, trader, others] = await ethers.getSigners();
+    const [owner, trader1, trader2, trader3, others] = await ethers.getSigners();
 
-    await seedAccount(orderBookDex, [daiToken, zrxToken], owner);
-    await seedAccount(orderBookDex, [daiToken, zrxToken], trader);
+    // Set Up Quote Ticker
+    await orderBookDex.connect(owner).setQuoteTicker(ethers.encodeBytes32String(daiSymbol));
+
+    // Seed Traders Account
+    await Promise.all(
+        [owner, trader1, trader2, trader3].map(async trader => {
+            await seedAccount(orderBookDex, [daiToken, zrxToken], trader)
+        })
+    );
+
+    // Place Orders
+    await orderBookDex.connect(trader1).placeOrder({
+        ticker: ethers.encodeBytes32String(zrxSymbol),
+        amount: ethers.parseUnits('1', 'ether'),
+        price: 10,
+        orderSide: ORDER_SIDE.BUY,
+        orderType: ORDER_TYPE.LIMIT
+    });
+
+    await orderBookDex.connect(trader1).placeOrder({
+        ticker: ethers.encodeBytes32String(zrxSymbol),
+        amount: ethers.parseUnits('2', 'ether'),
+        price: 20,
+        orderSide: ORDER_SIDE.BUY,
+        orderType: ORDER_TYPE.LIMIT
+    });
+
+    await orderBookDex.connect(trader2).placeOrder({
+        ticker: ethers.encodeBytes32String(zrxSymbol),
+        amount: ethers.parseUnits('3', 'ether'),
+        price: 30,
+        orderSide: ORDER_SIDE.SELL,
+        orderType: ORDER_TYPE.LIMIT
+    });
+
+    await orderBookDex.connect(trader3).placeOrder({
+        ticker: ethers.encodeBytes32String(zrxSymbol),
+        amount: ethers.parseUnits('4', 'ether'),
+        price: 40,
+        orderSide: ORDER_SIDE.SELL,
+        orderType: ORDER_TYPE.LIMIT
+    });
 
     const adresses = {
         OBDex: orderBookDexAddress,
