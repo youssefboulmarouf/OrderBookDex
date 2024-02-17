f90611b6f7d2617f19fbe068c749b2400c97163f
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-02-16 21:14:00 -0500

    added tabs in trades

--- client/src/components/trades/Trades.tsx
+++ client/src/components/trades/Trades.tsx
@@ -1,8 +1,24 @@
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
 import './trades.css';
 
 function Trades() {
     return (
-        <div className="default-box-layout trades">TRADES</div>
+        <div className="default-box-layout trades">
+            <div className='title-box'>Orders & Trades</div>
+            <div className='inner-box'>
+                <Tabs defaultActiveKey="orders" className="mb-3">
+                    <Tab eventKey="orders" title="My Orders">
+                        Tab content for Home
+                    </Tab>
+                    <Tab eventKey="trades" title="My Trades">
+                        Tab content for Profile
+                    </Tab>
+                </Tabs>
+            </div>
+            
+        </div>
     );
 }
 
