ca8a6613d64c9ba3659c8da24a231f951c9fa3db
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-02-18 12:01:34 -0500

    added order types and order enums

--- client/src/components/common/common-props.tsx
+++ client/src/components/common/common-props.tsx
@@ -10,4 +10,21 @@ interface TokenDexBalance {
     locked: BigInt;
 }
 
-export type { TokenProps, TokenDexBalance };
+enum ORDER_SIDE { BUY, SELL }
+enum ORDER_TYPE { MARKET, LIMIT }
+
+
+interface Order {
+    id: number;
+    traderAddress: string;
+    orderSide: ORDER_SIDE;
+    orderType: ORDER_TYPE;
+    ticker: string;
+    amount: BigInt;
+    fills: number[];
+    price: number;
+    date: string;
+}
+
+export type { TokenProps, TokenDexBalance, Order };
+export { ORDER_SIDE, ORDER_TYPE }
