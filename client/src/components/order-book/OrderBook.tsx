c46e64da8af1e52401ec685af2d5c8e4b87798cb
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-02-18 17:00:26 -0500

    created order book component

--- client/src/components/order-book/OrderBook.tsx
+++ client/src/components/order-book/OrderBook.tsx
@@ -1,8 +1,109 @@
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import { Table } from 'react-bootstrap';
+import { TokenProps, Order, ORDER_SIDE } from '../common/common-props';
+import { Signer, ethers } from 'ethers';
+import OrderBookDexContract from '../../services/OrderBookDexContract';
+import { useEffect, useState } from 'react';
 import './order-book.css';
 
-function OrderBook() {
+interface OrderBookProps {
+    selectedAsset: TokenProps;
+    account: Signer;
+    orderBookDexContract: OrderBookDexContract;
+}
+
+interface AggregatedOrder {
+    price: number;
+    totalAmount: BigInt;
+    orders: Order[];
+}
+
+const OrderBook: React.FC<OrderBookProps> = (props) => {
+
+    const [aggregatedBuyOrders, setAggregatedBuyOrders] = useState<AggregatedOrder[]>([]);
+    const [aggregatedSellOrders, setAggregatedSellOrders] = useState<AggregatedOrder[]>([]);
+
+    const loadOrders = async () => {
+        const buyOrders = await props.orderBookDexContract.getOrders(props.selectedAsset, ORDER_SIDE.BUY);
+        const sellOrders = await props.orderBookDexContract.getOrders(props.selectedAsset, ORDER_SIDE.SELL);
+        setAggregatedBuyOrders(aggregateOrdersByPrice(buyOrders))
+        setAggregatedSellOrders(aggregateOrdersByPrice(sellOrders))
+    }
+
+    function aggregateOrdersByPrice(orders: Order[]): AggregatedOrder[] {
+        orders = [...orders].reverse();
+    
+        const aggregationMap = new Map<number, AggregatedOrder>();
+    
+        orders.forEach(order => {
+            const filledAmount = order.fills.reduce((acc, fill) => BigInt(acc.toString()) + BigInt(fill), BigInt(0));
+            const remainingAmount = BigInt(order.amount.toString()) - filledAmount;
+    
+            if (!aggregationMap.has(order.price)) {
+                aggregationMap.set(order.price, {
+                    price: order.price,
+                    totalAmount: BigInt(0),
+                    orders: []
+                });
+            }
+            
+            const aggregatedOrder = aggregationMap.get(order.price);
+            if (aggregatedOrder) {
+                aggregatedOrder.totalAmount = BigInt(aggregatedOrder.totalAmount.toString()) + remainingAmount;
+                aggregatedOrder.orders.push(order);
+            }
+        });
+    
+        return Array.from(aggregationMap.values());
+    }
+
+    useEffect(() => {
+        loadOrders();
+    }, [props.selectedAsset]);
+
     return (
-        <div className="default-box-layout order-book">ORDER BOOK</div>
+        <div className="default-box-layout order-book">
+            <div className='title-box'>ORDER BOOK</div>
+            <div className='inner-box'>
+                <Tabs defaultActiveKey="orderBook" fill>
+                    <Tab eventKey="orderBook" title="Order Book">
+                        <div>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th>Price</th>
+                                        <th>AMount</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {aggregatedSellOrders.map(order => (
+                                    <tr key={order.price}>
+                                        <td>{order.price.toString()}</td>
+                                        <td>{ethers.formatEther(order.totalAmount.toString())}</td>
+                                    </tr>
+                                ))}
+
+                                <tr><td></td><td></td></tr>
+
+                                {aggregatedBuyOrders.map(order => (
+                                    <tr key={order.price} >
+                                        <td>{order.price.toString()}</td>
+                                        <td>{ethers.formatEther(order.totalAmount.toString())}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                                <tbody>
+                                </tbody>
+                            </Table>
+                        </div>
+                    </Tab>
+                    <Tab eventKey="allTrades" title="All Trades">
+                        Tab content for Profile
+                    </Tab>
+                </Tabs>
+            </div>
+        </div>
     );
 }
 
