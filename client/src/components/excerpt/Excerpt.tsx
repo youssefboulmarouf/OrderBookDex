158357d95a3a58b9cda9ec8d13b40c23b3a2fe9a
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-02-20 20:59:07 -0500

    added ability to connect from excerpt

--- client/src/components/excerpt/Excerpt.tsx
+++ client/src/components/excerpt/Excerpt.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import './excerpt.css';
 
-const Excerpt: React.FC = () => {
+interface ExcerptProps {
+    connectWallet: () => void;
+}
+
+const Excerpt: React.FC<ExcerptProps> = (props) => {
     return(
         <>
         <div className="content">
@@ -11,9 +15,9 @@ const Excerpt: React.FC = () => {
                     <h1>OrderBookDex</h1>
                 </div>
                 <div className='excerpt-body'>
-                    <p>Is a mono-quote token Order Book trading platform concieved for learning and testing only.</p>
+                    <p>A mono-quote token Order Book trading platform concieved for learning and testing only.</p>
                 </div>
-                <Button variant='warning'>Connect Wallet</Button>
+                <Button variant='warning' onClick={props.connectWallet}>Connect Wallet</Button>
             </div>
         </div>
         </>
