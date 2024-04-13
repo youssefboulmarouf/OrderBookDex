c69df6aa50e4e40a3d7ca113b9f4d49961c95139
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-04-13 16:02:49 -0400

    added some attribute to app context

--- client/src/AppContext.tsx
+++ client/src/AppContext.tsx
@@ -1,15 +1,20 @@
 import React, { createContext, useContext, ReactNode, useState, useCallback, useEffect } from 'react';
 import OrderBookDexContract from './services/OrderBookDexContract';
 import { Signer } from 'ethers';
-import { ORDER_SIDE, Order, TokenProps } from './components/common/common-props';
+import { NewTradeEvent, ORDER_SIDE, Order, TokenProps } from './components/common/common-props';
 
 interface AppContextType {
     tokens: TokenProps[];
     selectedAsset: TokenProps;
+    quoteToken: TokenProps;
     account: Signer;
+    buySellButton: string;
     orderBookDexContract: OrderBookDexContract;
     buyOrders: Order[],
     sellOrders: Order[],
+    marketPrice: bigint,
+    tokenTrades: NewTradeEvent[],
+    setMarketPrice: (price: bigint) => void,
     triggerRefresh: () => void;
 }
 
@@ -26,40 +31,48 @@ export const useAppContext = () => {
 interface AppProviderProps {
     children: ReactNode;
     tokens: TokenProps[];
+    quoteToken: TokenProps;
     selectedAsset: TokenProps;
+    buySellButton: string;
     account: Signer;
     orderBookDexContract: OrderBookDexContract;
 }
 
 export const AppProvider: React.FC<AppProviderProps> = (
-    { children, tokens, selectedAsset, account, orderBookDexContract }
+    { children, tokens, selectedAsset, quoteToken, buySellButton, account, orderBookDexContract }
 ) => {
+    const [marketPrice, setMarketPrice] = useState<bigint>(BigInt(0));
     const [buyOrders, setBuyOrders] = useState<Order[]>([]);
     const [sellOrders, setSellOrders] = useState<Order[]>([]);
+    const [tokenTrades, setTokenTrades] = useState<NewTradeEvent[]>([])
 
-    const triggerRefresh = useCallback(() => {
-        loadOrders();
+    const triggerRefresh = useCallback(async () => {
+        await loadOrdersAndTrades();
     }, []);
 
-    const loadOrders = async () => {
-        const buys = await orderBookDexContract.getOrders(selectedAsset, ORDER_SIDE.BUY)
-        setBuyOrders(buys);
-        const sells = await orderBookDexContract.getOrders(selectedAsset, ORDER_SIDE.SELL)
-        setSellOrders(sells);
+    const loadOrdersAndTrades = async () => {
+        setBuyOrders(await orderBookDexContract.getOrders(selectedAsset, ORDER_SIDE.BUY));
+        setSellOrders(await orderBookDexContract.getOrders(selectedAsset, ORDER_SIDE.SELL));
+        setTokenTrades(await orderBookDexContract.getNewTradeEvents());
     };
 
     useEffect(() => {
-        loadOrders();
+        loadOrdersAndTrades();
     }, [selectedAsset]);
 
     return (
         <AppContext.Provider value={{ 
             tokens, 
             selectedAsset, 
+            quoteToken,
             account, 
             orderBookDexContract, 
+            buySellButton,
             buyOrders, 
             sellOrders, 
+            marketPrice,
+            tokenTrades,
+            setMarketPrice,
             triggerRefresh 
         }}>
             {children}
