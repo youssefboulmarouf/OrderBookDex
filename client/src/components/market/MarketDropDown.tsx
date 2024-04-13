20569dc4e2e2cb9bd824fd81a0ef5f6bb900c143
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-04-13 16:08:18 -0400

    created market dropdown component

---
+++ client/src/components/market/MarketDropDown.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { TokenProps } from '../common/common-props';
+import { ethers } from 'ethers';
+
+interface MarketDropdownProps {
+    tokens: TokenProps[];
+    assetToken: TokenProps | undefined;
+    //setAssetToken: (assetToken: TokenProps) => void;
+}
+
+const MarketDropDown: React.FC<MarketDropdownProps> = (props) => {
+    const [selectedAsset, setSelectedAsset] = useState<string>('');
+
+    useEffect(() => {
+        const init = () => {
+            if(props.assetToken != undefined) {
+                setSelectedAsset(ethers.decodeBytes32String(props.assetToken.ticker))
+            }
+        }
+
+        init();
+    }, [props.assetToken])
+
+    useEffect(() => {
+        const filterToken = () => {
+            const token: TokenProps[] = props.tokens.filter(token => 
+                ethers.decodeBytes32String(token.ticker) == selectedAsset
+            );
+
+            if (token.length > 0) {
+                //props.setAssetToken(token[0]);
+                console.log(0)
+            }
+        }
+
+        filterToken();
+    }, [selectedAsset])
+    
+    return (
+        <Dropdown className="transparent-dropdown" onSelect={(item) => setSelectedAsset((item !== null) ? item : '')}>
+            <Dropdown.Toggle>
+                <span className='dropdown-toggle-left-side'>
+                    {selectedAsset ? `${selectedAsset} ` : ''}
+                </span>
+                <span className='dropdown-toggle-right-side'>Asset</span>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                {props.tokens.map((token) => (
+                    <Dropdown.Item key={token.ticker} eventKey={ethers.decodeBytes32String(token.ticker)}>
+                        {ethers.decodeBytes32String(token.ticker)}
+                    </Dropdown.Item>
+                ))}
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+}
+
+export default MarketDropDown;
