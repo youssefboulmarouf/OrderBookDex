a0367de7a760dc7c2fc60219b305bacfc660126b
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-04-13 16:08:38 -0400

    revamped navbar component

--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -4,47 +4,70 @@ import AdminSection from '../admin-section/AdminSection';
 import OrderBookDexContract from '../../services/OrderBookDexContract';
 
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import { Container } from 'react-bootstrap';
-import { GearFill } from 'react-bootstrap-icons';
+import { CashCoin, GearFill, WalletFill } from 'react-bootstrap-icons';
 
 import './navbar.css';
+import SeedWallet from '../seed-wallet/SeedWallet';
+import { Signer, ethers } from 'ethers';
 
 interface NavBarProps {
     connectWallet: () => void;
     isReady: () => boolean;
     isAdmin: boolean;
     orderBookDexContract: OrderBookDexContract | undefined;
+    signer: Signer | undefined;
 }
 
 const NavBar: React.FC<NavBarProps> = (props) => {
-    const [showAdminSection, setshowAdminSection] = useState(false);
+    const [showAdminSection, setShowAdminSection] = useState(false);
+    const [showSeedWallet, setShowSeedWallet] = useState(false);
 
     return (
         <>
-        <Navbar bg='accent1' expand='lg' fixed='top'>
+        <Navbar expand='lg' fixed='top'>
             <Container fluid>
                 <Navbar.Brand>OrderBookDEX</Navbar.Brand>
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <div className='ms-auto' hidden={props.isReady()}>
-                        <Button variant='warning' onClick={props.connectWallet}>Connect Wallet</Button>
-                    </div>
-                    {(props.isAdmin)
-                        ?   <div className='ms-auto'>
-                                <Button className='admin-button' variant='outline-secondary' onClick={() => setshowAdminSection(true)}>
-                                    <GearFill color='white' size={25}/>
-                                </Button>
-                            </div>
-                        : ''
-                    }
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="ms-auto">
+                        {(props.isReady())
+                            ? <>
+                                <div className='centered-buttons'>
+                                    <Button variant='info' onClick={() => setShowSeedWallet(true)}>
+                                        <CashCoin size={25}/><i> Seed Wallet</i>
+                                    </Button>
+                                    {(props.isAdmin)
+                                        ?   <Button className='admin-button' variant='info' onClick={() => setShowAdminSection(true)}>
+                                                <GearFill size={25}/><i> Admin Section</i>
+                                            </Button>
+                                        : ''
+                                    }
+                                </div>
+                            </>
+                            : <>
+                                <div className='centered-buttons'>
+                                    <Button variant='info' onClick={props.connectWallet}><WalletFill size={25}/><i> Connect Wallet</i></Button>
+                                </div>
+                            </>
+                        }
+                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
 
         <AdminSection 
             showAdminSection={showAdminSection} 
-            handleClose={() => setshowAdminSection(false)}
+            handleClose={() => setShowAdminSection(false)}
             orderBookDexContract={props.orderBookDexContract}
+        />
+        <SeedWallet 
+            showSeedWallet={showSeedWallet} 
+            handleClose={() => setShowSeedWallet(false)} 
+            orderBookDexContract={props.orderBookDexContract} 
+            signer={props.signer}
         />
         </>
     );
