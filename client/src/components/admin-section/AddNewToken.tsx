2bafcc684f09260c43ab5351f2abbd52b9f51e8e
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-04-13 16:04:35 -0400

    added div clasname

--- client/src/components/admin-section/AddNewToken.tsx
+++ client/src/components/admin-section/AddNewToken.tsx
@@ -10,36 +10,36 @@ interface AddNewTokenProps {
 
 const AddNewToken: React.FC<AddNewTokenProps> = (props) => {
     return (
-        <>
-        <div>
-            <h4>Add A New Token</h4>
+        <div className='add-new-token'>
+            <div>
+                <h4>Add A New Token</h4>
+            </div>
+            <Form onSubmit={(e) => {props.addToken(e)}}>
+                <FormGroup>
+                    <Form.Control 
+                        size='sm'
+                        type='text'
+                        placeholder='Token Ticker'
+                        name='ticker'
+                        value={props.token.ticker}
+                        onChange={props.handleTokenChange}
+                    />
+                </FormGroup>
+                <br />
+                <FormGroup>
+                    <Form.Control 
+                        size='sm'
+                        type='text'
+                        placeholder='Token Address'
+                        name='tokenAddress'
+                        value={props.token.tokenAddress}
+                        onChange={props.handleTokenChange}
+                    />
+                </FormGroup>
+                <br />
+                <Button variant='info' type="submit">Add New Token</Button>
+            </Form>
         </div>
-        <Form onSubmit={(e) => {props.addToken(e)}}>
-            <FormGroup>
-                <Form.Control 
-                    size='sm'
-                    type='text'
-                    placeholder='Token Ticker'
-                    name='ticker'
-                    value={props.token.ticker}
-                    onChange={props.handleTokenChange}
-                />
-            </FormGroup>
-            <br />
-            <FormGroup>
-                <Form.Control 
-                    size='sm'
-                    type='text'
-                    placeholder='Token Address'
-                    name='tokenAddress'
-                    value={props.token.tokenAddress}
-                    onChange={props.handleTokenChange}
-                />
-            </FormGroup>
-            <br />
-            <Button type="submit">Add New Token</Button>
-        </Form>
-        </>
     );
 }
 
