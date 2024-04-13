ce077dbdeeda04bb0cd6be0f85289e5eda0f20fb
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-04-13 16:01:00 -0400

    added hardhat optimizer configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
   paths: {
     artifacts: 'client/src/artifacts'
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      }
+    ]
   }
 };
 
